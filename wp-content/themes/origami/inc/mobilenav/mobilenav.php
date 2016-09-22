181b504ec38421ad3b38e839ff0cb20c2bdc8de1
satursunday <d.tongkampan@gmail.com>
2016-09-23 00:17:29 +0700

    mobile เปลี่ยนชื่อเมนู เป็น เมนูหลัก

--- wp-content/themes/origami/inc/mobilenav/mobilenav.php
+++ wp-content/themes/origami/inc/mobilenav/mobilenav.php
@@ -60,7 +60,7 @@ function siteorigin_mobilenav_nav_filter($nav_menu, $args){
 	$nav_menu .= '<'. $args->container . $id . $class . '>';
 
 	$text = array(
-		'navigate' => __( 'เมนู', 'origami' ),
+		'navigate' => __( 'เมนูหลัก', 'origami' ),
 		'back' => __( 'Back', 'origami' ),
 		'close' => __( 'Close', 'origami' ),
 	);
